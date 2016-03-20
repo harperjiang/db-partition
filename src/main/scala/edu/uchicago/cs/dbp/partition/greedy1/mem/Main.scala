d6bcfa9123cafb667dd0f2d0331526d43f27a827
Hao Jiang <harperjiang@msn.com>
2016-03-19 19:40:19 -0500

    Greedy Memory Version

--- src/main/scala/edu/uchicago/cs/dbp/partition/greedy1/mem/Main.scala
+++ src/main/scala/edu/uchicago/cs/dbp/partition/greedy1/mem/Main.scala
@@ -1,14 +1,23 @@
 package edu.uchicago.cs.dbp.partition.greedy1.mem
 
 import scala.io.Source
+import com.sun.glass.ui.Size
+import java.io.FileOutputStream
+import java.io.PrintWriter
 object Main extends App {
 
   var graph = new Graph();
+  var partition = 10
 
-  Source.fromFile("input").getLines().foreach(line => {
+  Source.fromFile("data/greedy1/tran_obj").getLines().foreach(line => {
     var parts = line.split("\t")
     graph.add(parts(0), parts(1))
   });
-  
-  
+
+  graph.partition(partition);
+
+  var output = new PrintWriter(new FileOutputStream("data/greedy1/partition"))
+  graph.labels.foreach(f => {
+    output.println("%s\t%d".format(f._1, f._2))
+  })
 }
