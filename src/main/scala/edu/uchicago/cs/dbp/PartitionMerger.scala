10634cb5dcc6dcce9ac079019447d62d97ca5267
Harper Jiang <harperjiang@msn.com>
2016-09-15 17:48:45 -0500

    PartitionMerger

--- src/main/scala/edu/uchicago/cs/dbp/PartitionMerger.scala
+++ src/main/scala/edu/uchicago/cs/dbp/PartitionMerger.scala
@@ -2,6 +2,7 @@ package edu.uchicago.cs.dbp
 
 import scala.collection.mutable.HashMap
 import scala.io.Source
+import scala.collection.mutable.ArrayBuffer
 
 object MergerParams {
   /**
@@ -40,24 +41,95 @@ class PartitionMerger {
       }
     })
 
+    var steps = new ArrayBuffer[MergeStep]();
+
     // Search for max value that add up to less than average
 
     var average = pmap.size / toSize
     var ceiling = average * MergerParams.threshold
 
-    var max = Double.MinValue
-    var index = (-1, -1)
-    for (i <- 0 until fromSize) {
-      for (j <- i + 1 until toSize) {
-        if (psize(i) + psize(j) < ceiling) {
-          var value = tm.get(i, j - 1)
-          if (value > max) {
-            index = (i, j)
+    var currentSize = fromSize
+
+    while (currentSize > toSize) {
+      var max = Double.MinValue
+      var index = (-1, -1)
+      for (i <- 0 until currentSize) {
+        for (j <- i + 1 until currentSize) {
+          if (psize(i) + psize(j) < ceiling) {
+            var value = tm.get(i, j - 1)
+            if (value > max) {
+              index = (i, j)
+            }
           }
         }
       }
+      // Merge partition i and j 
+      var step = new MergeStep(index._1, index._2)
+      steps += step
+      psize = step.convert(psize)
+      tm = step.convert(tm)
+      currentSize -= 1
     }
-    // Merge partition i and j 
+  }
+}
+
+/**
+ * <code>MergeStep</code> indicates a merge operation of two partition
+ */
+class MergeStep(i: Int, j: Int) {
+
+  def convert(psizes: Array[Int]): Array[Int] = {
+    var converted = Array.fill(psizes.length - 1)(0)
+
+    for (index <- 0 until converted.size) {
+      if (index < j) {
+        converted(index) = psizes(index)
+        if (index == i)
+          converted(index) += psizes(j)
+      } else {
+        converted(index) = psizes(index + 1)
+      }
+    }
+
+    converted
+  }
+
+  def convert(tm: TriangularMatrix): TriangularMatrix = {
+
+    var converted = new TriangularMatrix(tm.size - 1)
+
+    for (iid <- 0 until converted.size) {
+      for (jid <- i + 1 until converted.size) {
+        var value = -1
+
+        if (jid == i) {
+          value = tm.get(iid, i) + tm.get(iid, j)
+        } else if (iid == i) {
+          if (jid < j) {
+            value = tm.get(iid, j) + tm.get(jid, j)
+          } else {
+            value = tm.get(iid, j + 1) + tm.get(jid, j + 1)
+          }
+        } else {
+          // Not affected by merging
+          if (iid >= j) {
+            value = tm.get(iid + 1, jid + 1)
+          } else if (jid < j) {
+            value = tm.get(iid, jid)
+          } else {
+            value = tm.get(iid, jid + 1)
+          }
+        }
+
+        converted.set(iid, jid, value)
+      }
+    }
+
+    converted
+  }
+
+  def translate(pmap: Map[Int, Int]): Map[Int, Int] = {
+    null
   }
 }
 
---
+++ src/test/scala/edu/uchicago/cs/dbp/MergeStepTest.scala
@@ -0,0 +1,16 @@
+package edu.uchicago.cs.dbp
+
+import org.junit.Test
+
+class MergeStepTest {
+
+  @Test
+  def testConvertTM: Unit = {
+
+  }
+
+  @Test
+  def testConvertPsize: Unit = {
+
+  }
+}
