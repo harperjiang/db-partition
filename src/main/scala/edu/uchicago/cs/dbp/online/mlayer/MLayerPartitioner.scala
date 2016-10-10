93c3c43ad22c122c149ac98ecb8b99aed9c5b0c0
Cathy <Cathy@DESKTOP-Q5LIBVT>
2016-10-10 18:53:33 -0500

    Remove useless files

---
+++ src/main/scala/edu/uchicago/cs/dbp/online/mlayer/MLayerPartitioner.scala
@@ -0,0 +1,11 @@
+package edu.uchicago.cs.dbp.online.mlayer
+
+import edu.uchicago.cs.dbp.model.Edge
+import edu.uchicago.cs.dbp.AbstractPartitioner
+
+class MLayerPartitioner(nump:Int) extends AbstractPartitioner(nump) {
+  
+  def add(e: Edge) = {
+
+  }
+}
