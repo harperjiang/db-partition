ef711c2c316748ebffc5d7aa7581a4e595e06ac7
Harper Jiang <harperjiang@msn.com>
2016-10-13 16:23:30 -0500

    Remove Unnecessary Test

--- src/main/scala/edu/uchicago/cs/dbp/online/leopard/experiment/RunAll.scala
+++ src/main/scala/edu/uchicago/cs/dbp/online/leopard/experiment/RunAll.scala
@@ -4,7 +4,7 @@ object RunAll extends App {
   edu.uchicago.cs.dbp.online.leopard.experiment.astroph.RunLeopard.main(null)
   edu.uchicago.cs.dbp.online.leopard.experiment.berkstan.RunLeopard.main(null)
   edu.uchicago.cs.dbp.online.leopard.experiment.emailenron.RunLeopard.main(null)
-  edu.uchicago.cs.dbp.online.leopard.experiment.friendster.RunLeopard.main(null)
+  //edu.uchicago.cs.dbp.online.leopard.experiment.friendster.RunLeopard.main(null)
   edu.uchicago.cs.dbp.online.leopard.experiment.google.RunLeopard.main(null)
   edu.uchicago.cs.dbp.online.leopard.experiment.livejournal.RunLeopard.main(null)
   edu.uchicago.cs.dbp.online.leopard.experiment.notredame.RunLeopard.main(null)
